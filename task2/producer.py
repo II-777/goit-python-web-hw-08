bcb3d66122d08ce6ec6bfa3ddf2d8dbd173562ad
II-777 <45958058+II-777@users.noreply.github.com>
2023-11-28 06:31:13 -0500

    task 2/2

--- task2/producer.py
+++ task2/producer.py
@@ -4,9 +4,11 @@ from faker import Faker
 from models import Contact
 from mongoengine import connect
 
+
 def establish_mongo_connection():
     connect(db="hw08", alias="default",
             host="mongodb+srv://II-777:1234@cluster0.u2illjh.mongodb.net/?retryWrites=true&w=majority")
+
 
 def create_rabbitmq_connection():
     credentials = pika.PlainCredentials('guest', 'guest')
@@ -15,23 +17,26 @@ def create_rabbitmq_connection():
     channel = connection.channel()
     return connection, channel
 
+
 def publish_message(channel, contact):
     message = {'contact_id': str(contact.id)}
     channel.basic_publish(
         exchange='', routing_key='contacts_queue', body=json.dumps(message))
     print(f"[+] Message published {contact.email}")
 
+
 def close_rabbitmq_connection(connection):
     connection.close()
 
+
 def main():
     establish_mongo_connection()
-    
+
     fake = Faker()
-    
+
     rabbitmq_connection, channel = create_rabbitmq_connection()
     channel.queue_declare(queue='contacts_queue')
-    
+
     print("[+] Producer script started. Publishing messages to RabbitMQ...")
 
     for _ in range(10):
@@ -43,6 +48,7 @@ def main():
         publish_message(channel, contact)
 
     close_rabbitmq_connection(rabbitmq_connection)
+
 
 if __name__ == "__main__":
     main()
